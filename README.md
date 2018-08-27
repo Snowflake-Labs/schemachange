c19fcc5542963f2ae00a7b9f8cea2310471bb73c
jweakley <j.weakley@nib.com.au>
2018-08-27 20:28:57 +1000

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,74 @@
 # snowchange
+Snowchange is a simple python script which helps manage schema changes for the Snowflake data warehouse.
+
+When combined with a version control system and a CI/CD tool, schema changes can be approved and deployed through a pipeline using modern software delivery practises.
+
+## Usage
+
+It's just a single python file named snowchange.py. Create a repository, copy it in there, and follow the example databases structure to get started.
+
+### Preparation
+
+The function expects a directory structure like the following to exist:
+```
+(rootFolder)
+ |
+ |-> databases
+      |-> database1
+           |->schema1
+           |   |-> 20180825_create_customer_table.sql
+           |   |-> 20180826_create_sales_table.sql
+           |->schema2
+           |   |-> 20180715_create_flights_table.sql
+           |   |-> 20180805_create_bookings_table.sql
+```
+
+The database name in the directory structure is combined with the environment name, so that a single snowflake  account can include all environments.
+
+Every database will have a table automatically created to track the applying of these change sets.
+
+You will need a user account that has the ```CREATE DATABASE``` account-level permission. 
+
+It is recommended that the script be triggered to run via repository webhooks, however it can safely be re-ran repeatedly.
+
+### Running the script
+
+If your build agent has python 3 installed, the script can be ran like so:
+```
+pip install --upgrade snowflake-connector-python
+python snowchange.py -e $ENVIRONMENT_NAME -a $SNOWFLAKE_ACCOUNT -u $SNOWFLAKE_USER -r $SNOWFLAKE_ROLE -w $SNOWFLAKE_WAREHOUSE --snowflak
+e-region $SNOWFLAKE_REGION --repo-revision $GIT_COMMIT_REF
+```
+it is expected that the environment variable SNOWSQL_PWD be set prior to calling the script, you should make this available to your build agent in some secure fashion.
+
+You'll need to map between the branch name and the target environment name, e.g. 
+
+Or if you prefer docker, set the environment variables and run like so:
+```
+docker run -it --rm \
+  -v "$PWD":/usr/src/snowchange \
+  -w /usr/src/snowchange \
+  -e SNOWFLAKE_ACCOUNT \
+  -e SNOWFLAKE_USER \
+  -e SNOWFLAKE_ROLE \
+  -e SNOWFLAKE_WAREHOUSE \
+  -e SNOWFLAKE_REGION \
+  -e SNOWFLAKE_REGION \
+  -e SNOWSQL_PWD \
+  --name snowchange-script \
+  python:3 /bin/bash -c "pip install --upgrade snowflake-connector-python && python snowchange.py -e $ENVIRONMENT_NAME -a $SNOWFLAKE_ACCOUNT -u $SNOWFLAKE_USER -r $SNOWFLAKE_ROLE -w $SNOWFLAKE_WAREHOUSE --snowflake-region $SNOWFLAKE_REGION --repo-revision $BUILDKITE_COMMIT"
+```
+
+## The script in context
+
+Here's an example configuration, where pull requests move changes between environments.
+
+![diagram](diagram.png "Diagram")
+
+## Notes
+
+In the interest of safety, if databases/schemas are removed from the repository, they will not automatically be removed in snowflake. The script only giveth, it does not taketh away.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
---
+++ snowchange.py
@@ -0,0 +1,116 @@
+import os
+import snowflake.connector
+import re
+import argparse
+
+
+def snowchange(environment_name,snowflake_account,snowflake_user,snowflake_role,snowflake_warehouse,snowflake_region,repo_revision,root_folder,database_naming_convention,verbose):
+  if os.environ["SNOWSQL_PWD"] is None:
+    raise ValueError("The SNOWSQL_PWD environment variable has not been defined")
+  root_folder=os.path.abspath(root_folder)
+  if not os.path.isdir(root_folder):
+    raise ValueError("Invalid root folder: %s" % root_folder)
+  databases_folder=os.path.join(root_folder,'databases')
+  if not os.path.isdir(root_folder):
+    raise ValueError("Root folder does not contain a 'databases' subfolder")
+  print("root_folder: %s" % root_folder)
+  print("environment_name: %s" % environment_name)
+  print("database_naming_convention: %s" % database_naming_convention)
+  os.environ["SNOWFLAKE_ACCOUNT"]=snowflake_account
+  os.environ["SNOWFLAKE_USER"]=snowflake_user
+  os.environ["SNOWFLAKE_ROLE"]=snowflake_role
+  os.environ["SNOWFLAKE_WAREHOUSE"]=snowflake_warehouse
+  os.environ["SNOWFLAKE_REGION"] = snowflake_region
+  os.environ["SNOWFLAKE_AUTHENTICATOR"]='snowflake'
+
+  for database_folder in os.scandir(databases_folder):
+    if database_folder.is_dir():
+      print("Processing database %s" % database_folder.name)
+      snowflake_database_name=database_naming_convention.format(database_folder.name,environment_name)
+      print("snowflake database name: %s" % snowflake_database_name)
+        
+      create_database_if_missing(snowflake_database_name,verbose)
+
+      for schema_folder in os.scandir(database_folder):
+        schema_name=schema_folder.name
+        print("Processing schema %s" % schema_name)
+        create_schema_if_missing(snowflake_database_name,schema_name,verbose)
+        create_schema_history_table_if_missing(snowflake_database_name,schema_name,verbose)
+        schema_history=fetch_schema_history(snowflake_database_name,schema_name,verbose)
+        if verbose:
+            print("Schema history: %s" % schema_history)
+        for schema_changeset_file in os.scandir(schema_folder):
+          if re.match(r'\d{8}_\w*\.sql',schema_changeset_file.name):
+            print("Processing changeset file %s" % schema_changeset_file.name)
+            if schema_changeset_file.name in schema_history:
+              print("Skipping changeset file %s as it has already been applied" % schema_changeset_file.name)
+            else:
+              print("Applying changeset file %s" % schema_changeset_file.name)
+              apply_schema_file(snowflake_database_name, schema_name, schema_changeset_file, repo_revision,
+                                  verbose)
+          else:
+            print("Skipping file %s as the name is invalid" % schema_changeset_file.name)
+  print("Completed successfully")
+
+def execute_snowflake_query(snowflake_database,snowflake_schema,query,verbose):
+  con = snowflake.connector.connect(
+    user=os.environ["SNOWFLAKE_USER"],
+    account=os.environ["SNOWFLAKE_ACCOUNT"],
+    role=os.environ["SNOWFLAKE_ROLE"],
+    warehouse=os.environ["SNOWFLAKE_WAREHOUSE"],
+    database=snowflake_database,
+    schema=snowflake_schema,
+    region=os.environ["SNOWFLAKE_REGION"],
+    authenticator=os.environ["SNOWFLAKE_AUTHENTICATOR"],
+    password=os.environ["SNOWSQL_PWD"]
+  )
+  if verbose:
+      print("SQL query: %s" % query)
+  try:
+    return con.execute_string(query)
+  finally:
+    con.close()
+
+def fetch_schema_history(snowflake_database,snowflake_schema,verbose):
+  query = 'select script_name from schema_history'
+  results = execute_snowflake_query(snowflake_database,snowflake_schema,query,verbose)
+  schema_history=[]
+  for cursor in results:
+    for row in cursor:
+      schema_history.append(row[0])
+  return schema_history
+
+def create_database_if_missing(database,verbose):
+  query = "create database if not exists {0}".format(database)
+  execute_snowflake_query('UTIL_DB',"",query,verbose)
+
+def create_schema_if_missing(snowflake_database,schema,verbose):
+  query = "create schema if not exists {0}".format(schema)
+  execute_snowflake_query(snowflake_database,schema,query,verbose)
+
+def create_schema_history_table_if_missing(snowflake_database,snowflake_schema,verbose):
+  query = "create table if not exists schema_history (script_name varchar,date_deployed datetime default current_timestamp::timestamp_ntz,repo_revision varchar)"
+  execute_snowflake_query(snowflake_database,snowflake_schema,query,verbose)
+
+def apply_schema_file(snowflake_database,snowflake_schema,file,repo_revision,verbose):
+  with open(file.path,'r') as content_file:
+    content=content_file.read()
+  query = "ALTER SESSION SET TRANSACTION_ABORT_ON_ERROR=TRUE;BEGIN TRANSACTION;{0};INSERT INTO schema_history (script_name,repo_revision) values ('{1}','{2}');COMMIT;".format(content,file.name,repo_revision)
+  execute_snowflake_query(snowflake_database,snowflake_schema,query,verbose)
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser("Apply schema changes to a Snowflake data warehouse")
+  parser.add_argument('-e', '--environment-name',type=str,help='The name of the environment (e.g. dev,test,prod)',required=True)
+  parser.add_argument('-a', '--snowflake-account',type=str,help='The name of the environment (e.g. dev,test,prod)',required=True)
+  parser.add_argument('-u', '--snowflake-user',type=str,help='The name of the environment (e.g. dev,test,prod)',required=True)
+  parser.add_argument('-r', '--snowflake-role',type=str,help='The name of the environment (e.g. dev,test,prod)',required=True)
+  parser.add_argument('-w', '--snowflake-warehouse',type=str,help='The name of the environment (e.g. dev,test,prod)',required=True)
+  parser.add_argument('--snowflake-region',type=str,help='The name of the environment (e.g. dev,test,prod)',required=True)
+  parser.add_argument('--repo-revision',type=str,help='The name of the environment (e.g. dev,test,prod)',required=True)
+  parser.add_argument('-f','--root-folder', default=".",type=str,help='The name of the environment (e.g. dev,test,prod)')
+  parser.add_argument('-n','--database-naming-convention', default="{0}_{1}",type=str,help='The name of the environment (e.g. dev,test,prod)')
+  
+  parser.add_argument('-v','--verbose', type=bool, default=False)
+  args = parser.parse_args()
+
+  snowchange(args.environment_name,args.snowflake_account,args.snowflake_user,args.snowflake_role,args.snowflake_warehouse,args.snowflake_region,args.repo_revision,args.root_folder,args.database_naming_convention,args.verbose)
