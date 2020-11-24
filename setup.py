c410fca4201e1f90aaeda363f9e4282bbc5025a9
Jeremiah Hansen <jeremiah.hansen@snowflake.com>
2020-11-24 13:50:03 -0800

    Updated description in package metadata

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     name="snowchange",
     version="2.4.0",
     author="jamesweakley/jeremiahhansen/zeitgeistf",
-    description="Python package for managing Snowflake schema.",
+    description="A Database Change Management tool for Snowflake",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Snowflake-Labs/snowchange",
