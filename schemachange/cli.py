cb73d39ac9b9bc0f6cccf09bfc6aefc3a9131579
Jonas Auel <auel.jonas@stadaarz.onmicrosoft.com>
2021-07-23 17:33:46 +0200

    bug fix script description

--- schemachange/cli.py
+++ schemachange/cli.py
@@ -201,7 +201,7 @@ def get_all_scripts_recursively(root_directory, verbose):
       elif script_type == 'D':
         script['script_description'] = delta_script_name_parts.group(2).replace('_', ' ').capitalize()
       else:
-        script_name_parts.group(3).replace('_', ' ').capitalize()
+        script['script_description'] = script_name_parts.group(3).replace('_', ' ').capitalize()
       
       all_files[file_name] = script
 
