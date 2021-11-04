4093f05054ad1ad0ecf10663957e86f1b7a812d5
Jarrad Whitaker <jwhitaker@officeworks.com.au>
2021-11-04 19:13:34 +1100

    fix tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,70 +3,127 @@ import sys
 import pytest
 import unittest.mock as mock
 import schemachange.cli 
+import tempfile
+from textwrap import dedent
 
-
-@pytest.mark.parametrize("args, expected", [
-    (["schemachange"], ('.', None, None, None, None, None, None, None, None, None, False, False, False, False)),
-    (["schemachange", "--config-folder", "test"], ('test', None, None, None, None, None, None, None, None, None, False, False, False, False)),
-    (["schemachange", "-f", '.'], ('.', '.', None, None, None, None, None, None, None, None, False, False, False, False)),     
-    (["schemachange", "--modules-folder", "modules-folder"], ('.', None, "modules-folder", None, None, None, None, None, None, None, False, False, False, False)),
-    (["schemachange", "--snowflake-account", "account"], ('.', None, None, "account", None, None, None, None, None, None, False, False, False, False)),
-    (["schemachange", "--snowflake-user", "user"], ('.', None, None, None, "user", None, None, None, None, None, False, False, False, False)),
-    (["schemachange", "--snowflake-role", "role"], ('.', None, None, None, None, "role", None, None, None, None, False, False, False, False)),
-    (["schemachange", "--snowflake-warehouse", "warehouse"], ('.', None, None, None, None, None, "warehouse", None, None, None, False, False, False, False)),
-    (["schemachange", "--snowflake-database", "database"], ('.', None, None, None, None, None, None, "database", None, None, False, False, False, False)),    
-    (["schemachange", "--change-history-table", "db.schema.table"], ('.', None, None, None, None, None, None, None, "db.schema.table", None, False, False, False, False)),    
-    (["schemachange", "--vars", '{"var1": "val"}'], ('.', None, None, None, None, None, None, None, None, {'var1' : 'val'}, False, False, False, False)),
-    (["schemachange", "--create-change-history-table"], ('.', None, None, None, None, None, None, None, None, None, True, False, False, False)),
-    (["schemachange", "--autocommit"], ('.', None, None, None, None, None, None, None, None, None, False, True, False, False)),
-    (["schemachange", "--verbose"], ('.', None, None, None, None, None, None, None, None, None, False, False, True, False)),
-    (["schemachange", "--dry-run"], ('.', None, None, None, None, None, None, None, None, None, False, False, False, True))       
-])
-def test_main_no_subcommand_given_arguments_make_sure_arguments_set_on_call( args, expected):
-    sys.argv = args
-    
-    with mock.patch("schemachange.cli.schemachange") as mock_schemachange:
-        schemachange.cli.main()
-        mock_schemachange.assert_called_once_with(*expected)
-
+DEFAULT_CONFIG = {
+    'root-folder': os.path.abspath('.'),
+    'modules-folder': None,
+    'snowflake-account': None,
+    'snowflake-user': None,
+    'snowflake-role':None,
+    'snowflake-warehouse': None,
+    'snowflake-database': None,
+    'change-history-table': None,
+    'vars': {},
+    'create-change-history-table': False,
+    'autocommit': False,
+    'verbose': False,
+    'dry-run': False,
+}
 
 @pytest.mark.parametrize("args, expected", [
-    (["schemachange", "deploy"], ('.', None, None, None, None, None, None, None, None, None, False, False, False, False)),
-    (["schemachange", "deploy", "--config-folder", "test"], ('test', None, None, None, None, None, None, None, None, None, False, False, False, False)),
-    (["schemachange", "deploy", "-f", '.'], ('.', '.', None, None, None, None, None, None, None, None, False, False, False, False)),     
-    (["schemachange", "deploy", "--modules-folder", "modules-folder"], ('.', None, "modules-folder", None, None, None, None, None, None, None, False, False, False, False)),
-    (["schemachange", "deploy", "--snowflake-account", "account"], ('.', None, None, "account", None, None, None, None, None, None, False, False, False, False)),
-    (["schemachange", "deploy", "--snowflake-user", "user"], ('.', None, None, None, "user", None, None, None, None, None, False, False, False, False)),
-    (["schemachange", "deploy", "--snowflake-role", "role"], ('.', None, None, None, None, "role", None, None, None, None, False, False, False, False)),
-    (["schemachange", "deploy", "--snowflake-warehouse", "warehouse"], ('.', None, None, None, None, None, "warehouse", None, None, None, False, False, False, False)),
-    (["schemachange", "deploy", "--snowflake-database", "database"], ('.', None, None, None, None, None, None, "database", None, None, False, False, False, False)),    
-    (["schemachange", "deploy", "--change-history-table", "db.schema.table"], ('.', None, None, None, None, None, None, None, "db.schema.table", None, False, False, False, False)),    
-    (["schemachange", "deploy", "--vars", '{"var1": "val"}'], ('.', None, None, None, None, None, None, None, None, {'var1' : 'val'}, False, False, False, False)),
-    (["schemachange", "deploy", "--create-change-history-table"], ('.', None, None, None, None, None, None, None, None, None, True, False, False, False)),
-    (["schemachange", "deploy", "--autocommit"], ('.', None, None, None, None, None, None, None, None, None, False, True, False, False)),
-    (["schemachange", "deploy", "--verbose"], ('.', None, None, None, None, None, None, None, None, None, False, False, True, False)),
-    (["schemachange", "deploy", "--dry-run"], ('.', None, None, None, None, None, None, None, None, None, False, False, False, True))
-       
+    (["schemachange"], DEFAULT_CONFIG),
+    (["schemachange", "deploy"], DEFAULT_CONFIG),
+    (["schemachange", "deploy", "-f", '.'],
+        {**DEFAULT_CONFIG, 'root-folder':os.path.abspath('.')}),
+    (["schemachange", "deploy", "--snowflake-account", "account"],
+        {**DEFAULT_CONFIG, 'snowflake-account': 'account'}),
+    (["schemachange", "deploy", "--snowflake-user", "user"],
+        {**DEFAULT_CONFIG, 'snowflake-user': 'user'}),
+    (["schemachange", "deploy", "--snowflake-role", "role"],
+        {**DEFAULT_CONFIG, 'snowflake-role': 'role'}),
+    (["schemachange", "deploy", "--snowflake-warehouse", "warehouse"],
+        {**DEFAULT_CONFIG, 'snowflake-warehouse': 'warehouse'}),
+    (["schemachange", "deploy", "--snowflake-database", "database"],
+        {**DEFAULT_CONFIG, 'snowflake-database': 'database'}),
+    (["schemachange", "deploy", "--change-history-table", "db.schema.table"],
+        {**DEFAULT_CONFIG, 'change-history-table': 'db.schema.table'}),
+    (["schemachange", "deploy", "--vars", '{"var1": "val"}'],
+        {**DEFAULT_CONFIG, 'vars': {'var1' : 'val'},}),
+    (["schemachange", "deploy", "--create-change-history-table"],
+        {**DEFAULT_CONFIG, 'create-change-history-table': True}),
+    (["schemachange", "deploy", "--autocommit"],
+        {**DEFAULT_CONFIG, 'autocommit': True}),
+    (["schemachange", "deploy", "--verbose"],
+        {**DEFAULT_CONFIG, 'verbose': True}),
+    (["schemachange", "deploy", "--dry-run"],
+        {**DEFAULT_CONFIG, 'dry-run': True}),
 ])
+
 def test_main_deploy_subcommand_given_arguments_make_sure_arguments_set_on_call( args, expected):
     sys.argv = args
     
-    with mock.patch("schemachange.cli.schemachange") as mock_schemachange:
+    with mock.patch("schemachange.cli.deploy_command") as mock_deploy_command:
         schemachange.cli.main()
-        mock_schemachange.assert_called_once_with(*expected)
+        mock_deploy_command.assert_called_once()
+        [config,], _call_kwargs = mock_deploy_command.call_args
+        assert config == expected
 
+@pytest.mark.parametrize("args, to_mock, expected_args", [
+    (["schemachange", "deploy", "--config-folder", "DUMMY"],
+        "schemachange.cli.deploy_command",
+        ({**DEFAULT_CONFIG, 'snowflake-account': 'account'},)),
+    (["schemachange", "render", "script.sql", "--config-folder", "DUMMY"],
+        "schemachange.cli.render_command",
+        ({**DEFAULT_CONFIG, 'snowflake-account': 'account'}, "script.sql"))
+])
+def test_main_deploy_config_folder(args, to_mock, expected_args):
+    with tempfile.TemporaryDirectory() as d:
+        with open(os.path.join(d, 'schemachange-config.yml'), 'wt') as f:
+            f.write(dedent('''
+                snowflake-account: account
+            '''))
+
+        args[args.index("DUMMY")] = d
+        sys.argv = args
+
+        with mock.patch(to_mock) as mock_command:
+            schemachange.cli.main()
+            mock_command.assert_called_once()
+            call_args, _call_kwargs = mock_command.call_args
+            assert call_args == expected_args
+
+
+@pytest.mark.parametrize("args, to_mock, expected_args", [
+    (["schemachange", "deploy", "--modules-folder", "DUMMY"],
+        "schemachange.cli.deploy_command",
+        ({**DEFAULT_CONFIG, 'modules-folder': 'DUMMY'},)),
+    (["schemachange", "render", "script.sql", "--modules-folder", "DUMMY"],
+        "schemachange.cli.render_command",
+        ({**DEFAULT_CONFIG, 'modules-folder': 'DUMMY'}, "script.sql"))
+])
+def test_main_deploy_modules_folder(args, to_mock, expected_args):
+    with tempfile.TemporaryDirectory() as d:
+
+        args[args.index("DUMMY")] = d
+        expected_args[0]['modules-folder'] = d
+        sys.argv = args
+
+        with mock.patch(to_mock) as mock_command:
+            schemachange.cli.main()
+            mock_command.assert_called_once()
+            call_args, _call_kwargs = mock_command.call_args
+            assert call_args == expected_args
 
 @pytest.mark.parametrize("args, expected", [
-    (["schemachange", "render", "script.sql"], ('.', None, None, None, False, "script.sql")),
-    (["schemachange", "render", "--config-folder", "test", "script.sql"], ("test", None, None, None, False, "script.sql")),
-    (["schemachange", "render", "--root-folder", '.', "script.sql"], ('.', ".", None, None, False, "script.sql")),
-    (["schemachange", "render", "--modules-folder", "modules-folder", "script.sql"], ('.', None, "modules-folder", None, False, "script.sql")),
-    (["schemachange", "render", "--vars", '{"var1": "val"}', "script.sql"], ('.', None, None, {'var1' : 'val'}, False, "script.sql")),
-    (["schemachange", "render", "--verbose", "script.sql"], ('.', None, None, None, True, "script.sql")),      
+    (["schemachange", "render", "script.sql"],
+        ({**DEFAULT_CONFIG}, "script.sql")),
+    # (["schemachange", "render", "--config-folder", "test", "script.sql"],
+    #     ("test", None, None, None, False, "script.sql")),
+    (["schemachange", "render", "--root-folder", '.', "script.sql"],
+        ({**DEFAULT_CONFIG, 'root-folder': os.path.abspath('.')}, "script.sql")),
+    #(["schemachange", "render", "--modules-folder", "modules-folder", "script.sql"], ('.', None, "modules-folder", None, False, "script.sql")),
+    (["schemachange", "render", "--vars", '{"var1": "val"}', "script.sql"],
+        ({**DEFAULT_CONFIG, 'vars': {"var1": "val"}}, "script.sql")),
+    (["schemachange", "render", "--verbose", "script.sql"],
+        ({**DEFAULT_CONFIG, 'verbose': True}, "script.sql")),
 ])
 def test_main_render_subcommand_given_arguments_make_sure_arguments_set_on_call( args, expected):
     sys.argv = args
     
     with mock.patch("schemachange.cli.render_command") as mock_render_command:
         schemachange.cli.main()
-        mock_render_command.assert_called_once_with(*expected)
+        mock_render_command.assert_called_once()
+        call_args, _call_kwargs = mock_render_command.call_args
+        assert call_args == expected
