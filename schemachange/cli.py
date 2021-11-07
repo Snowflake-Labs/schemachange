cb94cc6cee5abe7f61df16e8b7f19085e22f41ad
Jarrad Whitaker <akdor1154@gmail.com>
2021-11-07 21:13:00 +1100

    3.3.1

--- schemachange/cli.py
+++ schemachange/cli.py
@@ -21,7 +21,7 @@ from cryptography.hazmat.primitives.asymmetric import dsa
 from cryptography.hazmat.primitives import serialization
 
 # Set a few global variables here
-_schemachange_version = '3.3.0'
+_schemachange_version = '3.3.1'
 _config_file_name = 'schemachange-config.yml'
 _metadata_database_name = 'METADATA'
 _metadata_schema_name = 'SCHEMACHANGE'
