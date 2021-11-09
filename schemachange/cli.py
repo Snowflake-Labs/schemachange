e169b324a9a138af8cdc9df0abe07203f3d89f82
Taras Savko <taras.savko@grainger.com>
2021-11-09 12:50:48 +0000

    Add env_var support to migration templates

--- schemachange/cli.py
+++ schemachange/cli.py
@@ -67,7 +67,7 @@ class JinjaTemplateProcessor:
     else:
       loader = jinja2.FileSystemLoader(project_root)
 
-    self.__environment = jinja2.Environment(loader=loader, undefined=jinja2.StrictUndefined, autoescape=True)
+    self.__environment = jinja2.Environment(loader=loader, undefined=jinja2.StrictUndefined, autoescape=True, extensions=[JinjaEnvVar])
     self.__project_root = project_root
 
   def list(self):
@@ -75,7 +75,7 @@ class JinjaTemplateProcessor:
 
   def override_loader(self, loader: jinja2.BaseLoader):
     # to make unit testing easier
-    self.__environment = jinja2.Environment(loader=loader, undefined=jinja2.StrictUndefined, autoescape=True)
+    self.__environment = jinja2.Environment(loader=loader, undefined=jinja2.StrictUndefined, autoescape=True, extensions=[JinjaEnvVar])
 
   def render(self, script: str, vars: Dict[str, Any], verbose: bool) -> str:
     if not vars:
