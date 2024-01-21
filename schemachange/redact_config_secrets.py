61795f5243ee52a65aa237b2173040b7be03f760
Zane Clark <T001ZC@kyfb.com>
2024-01-21 10:15:28 -0800

    feat: test redact_config_secrets

--- schemachange/redact_config_secrets.py
+++ schemachange/redact_config_secrets.py
@@ -29,11 +29,18 @@ def get_redact_config_secrets_processor(
                             sub_event_dict[sub_k] = sub_event_dict[sub_k].replace(
                                 secret, "*" * len(secret)
                             )
+                elif isinstance(sub_v, int):
+                    for secret in config_secrets:
+                        if secret in str(sub_v):
+                            sub_event_dict[sub_k] = str(sub_event_dict[sub_k]).replace(
+                                secret, "*" * len(secret)
+                            )
                 else:
                     warnings.warn(
                         "Unable to redact %(type)s log arguments in log: %(event)s"
                         % {"type": type(sub_v).__name__, "event": event_dict["event"]}
                     )
+                    return sub_event_dict
             return sub_event_dict
 
         return redact_dict(level=0, sub_event_dict=event_dict)
@@ -42,6 +49,9 @@ def get_redact_config_secrets_processor(
 
 
 def redact_config_secrets(config_secrets: set[str]) -> None:
+    if not config_secrets:
+        return
+
     cfg = structlog.get_config()
     redact_config_secrets_processor = get_redact_config_secrets_processor(
         config_secrets=config_secrets
