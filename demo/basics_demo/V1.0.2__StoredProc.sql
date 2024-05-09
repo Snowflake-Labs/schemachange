d10954e72d1ab6ca68b753a5d50b56f7d77485d1
Tiji Mathew <tiji.mathew@snowflake.com>
2024-05-09 01:36:08 -0400

    Testing stored procedure creation

--- demo/basics_demo/V1.0.2__StoredProc.sql
+++ demo/basics_demo/V1.0.2__StoredProc.sql
@@ -2,8 +2,11 @@
 -- Use the $$ ... $$ to mark the block and execute the code block successfully.
 -- The comment from a community user help find the root cause.
 -- Link to comment: https://github.com/Snowflake-Labs/schemachange/issues/212#issuecomment-2052187227
-$$
+
+CREATE OR REPLACE PROCEDURE output_message(message VARCHAR)
+RETURNS VARCHAR NOT NULL
+LANGUAGE SQL
+AS
 BEGIN
-    SELECT 1;
+  RETURN message;
 END;
-$$
