c0a25fd6138d0dcc2dcf91141dd8a8389dee8623
Tiji Mathew <tiji.mathew@snowflake.com>
2024-05-09 01:34:11 -0400

    workaround example for stored proc block

--- demo/basics_demo/V1.0.2__StoredProc.sql
+++ demo/basics_demo/V1.0.2__StoredProc.sql
@@ -1,3 +1,9 @@
+-- This block of code executes in Visual Studio Code but fails in Schemachange.
+-- Use the $$ ... $$ to mark the block and execute the code block successfully.
+-- The comment from a community user help find the root cause.
+-- Link to comment: https://github.com/Snowflake-Labs/schemachange/issues/212#issuecomment-2052187227
+$$
 BEGIN
     SELECT 1;
 END;
+$$
