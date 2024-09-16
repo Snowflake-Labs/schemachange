0ef9c54f13c0d96899e1e0f80c843bbbe66c5d12
Sam Nelson <sam.nelson@treasury4.com>
2024-09-16 08:59:28 -0600

    Working version of script dependency handler. Cleanup and refactoring

--- schemachange/cli.py
+++ schemachange/cli.py
@@ -17,12 +17,11 @@ import yaml
 import logging
 import logging.config
 import rich
-from rich import pretty
-from sqlglot import parse, parse_one, exp
+from sqlglot import parse, exp
 from sqlglot.optimizer.scope import build_scope
 import networkx as nx
 
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 from rich_argparse import RawTextRichHelpFormatter
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization
@@ -124,19 +123,6 @@ _log_okta_ep = "Okta Endpoint: %s"
 _log_current_session_id = "Current session ID: {current_session_id}"
 
 # endregion Global Variables
-
-
-class SQLGraph:
-    def __init__(self):
-        self.graph = {}
-
-    def add_edge(self, source, target):
-        if source not in self.graph:
-            self.graph[source] = []
-        self.graph[source].append(target)
-
-    def get_edges(self, source):
-        return self.graph.get(source, [])
 
 
 class JinjaEnvVar(jinja2.ext.Extension):
@@ -714,13 +700,14 @@ def deploy_command(config):
     ) = common_command_init(config)
 
     # Configure graph for R scripts
-    if config["r_script_graph"]:
-        # table_graph = nx.Graph()
-        table_graph = nx.DiGraph()
+    if config["script_deps"]:
+        script_graph = nx.DiGraph()
         changed_r_scripts = []
+        new_v_scripts = []
 
     scripts_skipped = 0
     scripts_applied = 0
+    scripts_applied_names = []
 
     checksum_current = None
     checksum_last = None
@@ -753,21 +740,20 @@ def deploy_command(config):
 
         # Apply only R scripts where the checksum changed compared to the last execution of snowchange
         if script_name[0] == "R":
-            if config["r_script_graph"]:
-                log.info("table graph")
-                # edges = get_script_table_graph_edges(content)
-                # log.info(edges)
-                get_script_table_graph_edges(content, table_graph, script_name)
-                # table_graph.add_edges_from = get_script_table_graph_edges(content)
-                # table_graph.add_edges_from(edges, script=script_name)
-                # table_graph.add_nodes_from(edges, script=script_name)
-
-            #        nx.write_network_text(table_graph)
-            #        print(f"Edges: {table_graph.number_of_edges()}")
-            #        print(f"Nodes: {table_graph.number_of_nodes()}")
+            if config["script_deps"]:
+                log.info("Building Repeatable script graph")
+                build_script_graph(content, script_graph, script_name)
 
             # Compute the checksum for the script
             checksum_current = hashlib.sha224(content.encode("utf-8")).hexdigest()
+        elif script_name[0] == "V":
+            if config["script_deps"]:
+                new_v_scripts.append(script_name)
+
+                log.info("Building Versioned script graph")
+                # Even though we will not re-run versioned scripts/tables,
+                # we still need to add them to the graph to analyze dependencies
+                build_script_graph(content, script_graph, script_name)
 
         # check if R file was already executed
         if r_scripts_checksum and (script_name in r_scripts_checksum):
@@ -781,11 +767,11 @@ def deploy_command(config):
             log.debug("Current Checksum %s" % checksum_current)
             scripts_skipped += 1
             continue
-        else:
+        elif checksum_current != checksum_last and script_name[0] == "R":
             log.info("Checksum changed for script %s" % script_name)
             log.debug("Current Checksum %s" % checksum_current)
             log.debug("Last Checksum %s" % checksum_last)
-            if config["r_script_graph"]:
+            if config["script_deps"]:
                 changed_r_scripts.append(script_name)
 
         log.info(_log_apply.format(**script))
@@ -793,6 +779,7 @@ def deploy_command(config):
             session.apply_change_script(script, content, change_history_table)
 
         scripts_applied += 1
+        scripts_applied_names.append(script_name)
 
     log.info(
         _log_apply_set_complete.format(
@@ -800,68 +787,64 @@ def deploy_command(config):
         )
     )
 
-    if config["r_script_graph"]:
-        log.info("Changed scripts: %s" % changed_r_scripts)
-        log.info("Script Graph")
-        print(f"Edges: {table_graph.number_of_edges()}")
-        print(f"Nodes: {table_graph.number_of_nodes()}")
-        nx.write_network_text(table_graph)
+    if config["script_deps"]:
+        log.info("Determining dependent scripts")
+        log.debug("Scripts already applied: %s" % scripts_applied_names)
+        log.debug("Changed repeatable scripts: %s" % changed_r_scripts)
+        log.debug("New versioned scripts: %s" % new_v_scripts)
 
-        # print(nx.dfs_successors(table_graph, "SILVER.DIM_ENTITIES"))
-        # print(nx.dfs_predecessors(table_graph, source="SILVER.DIM_ENTITIES"))
-
-        # print("SILVER.DIM_ENTITIES" in table_graph)
-
-        # print(table_graph.successors("SILVER.DIM_ENTITIES"))
-        for script in changed_r_scripts:
-            nodes = table_graph.nodes(data=True)
-            #            for successor in table_graph.successors(script):
-            #                print(successor)
-            #                print(nodes[successor])
-            #                # print(successor)
-            #                # print(successor["script"])
+        # Loop through all nodes corresponding to changed repeatable scripts
+        # and new versioned scripts to determine dependent scripts
+        dependent_scripts = []
+        for script in changed_r_scripts + new_v_scripts:
+            nodes = script_graph.nodes(data=True)
             for node in nodes:
                 if node[1].get("script", "") == script:
-                    print(node[0])
-                    print(node[1].get("script", ""))
-                    for successor in table_graph.successors(node[0]):
-                        print(f" |- {successor}")
-                        print(f" |- {nodes[successor]}")
-                        # print(successor)
-                        # print(successor["script"])
+                    log.info(f"Checking dependencies for script {script}")
+                    lines = "\n"
+                    for line in nx.generate_network_text(
+                        graph=script_graph,
+                        sources=[
+                            node[0],
+                        ],
+                        vertical_chains=True,
+                    ):
+                        lines += line + "\n"
 
+                    log.debug(f"[{node[0]}] Object Dependency Hierarchy: {lines}")
 
-#                print(node[1].get("script", ""))
-#                # if node["script"] == script:
-#                #    print(node)
+                    # Get ALL the successors of the node
+                    successors = nx.nodes(nx.dfs_tree(script_graph, node[0]))
 
-# for successor in table_graph.predecessors("SILVER.DIM_ENTITIES"):
-#    print(predecessor)
+                    for successor in successors:
+                        dependent_script = nodes[successor].get("script", "")
+                        if dependent_script:
+                            dependent_scripts.append(dependent_script)
 
-# print(table_graph.successors("SILVER.DIM_ENTITIES"))
+        # Remove duplicates and sort the list
+        dependent_scripts = set(sorted_alphanumeric(set(dependent_scripts)))
 
-#        for script in changed_r_scripts:
-#            print("Successors for: %s" % script)
-#            print(nx.dfs_successors(table_graph, script))
+        # Subtract applied scripts from dependent scripts
+        dependent_scripts.difference_update(set(scripts_applied_names))
+        dependent_scripts = sorted_alphanumeric(dependent_scripts)
+        log.info(
+            f"{len(dependent_scripts)} Final dependent scripts to apply: {dependent_scripts}"
+        )
 
-#       nodesAt5 = []
-#        edgesAt5 = []
-#        # for (p, d) in P.nodes(data=True):
-#        # for p, d in table_graph.edges(data=True):
-#        for edge in table_graph.edges(data=True):
-#            for script in changed_r_scripts:
-#                if edge["script"] == script:
-#                    edgesAt5.append(p)
-#
-#        print(edgesAt5)
+        for script_name in dependent_scripts:
+            log.info(f"Applying dependent script {script_name}")
+            script = all_scripts[script_name]
+            content = jinja_processor.render(
+                jinja_processor.relpath(script["script_full_path"]),
+                config["vars"],
+                config["verbose"],
+            )
+            if not config["dry_run"]:
+                session.apply_change_script(script, content, change_history_table)
 
-# files = nx.get_edge_attributes(table_graph, "script")
-# files = nx.get_node_attributes(table_graph, "script")
-# print(files)
-
-#        print(nx.write_network_text(table_graph))
-# nx.draw(table_graph, with_labels=True)
-# plt.show()
+        # Example for printing the graph via matplotlib chart
+        # nx.draw(script_graph, with_labels=True)
+        # plt.show()
 
 
 def baseline_command(config):
@@ -936,119 +919,59 @@ def baseline_command(config):
     log.info("Wrote %s baseline scripts to change history table" % scripts_baselined)
 
 
-def get_script_table_graph_edges(
+def build_script_graph(
     script_content: str, graph, script_name
 ) -> list[tuple[str, str]]:
     parsed_statements = parse(script_content, dialect="snowflake")
 
-    # Only process statements which create dynamic tables or views
+    """
+    Builds a graph of all of the DB objects created or altered in the script.
+    """
+
+    # Only process statements which create or alter
     for statement in parsed_statements:
-        if isinstance(statement, exp.Create):
-            #            formatted_sql_query = statement.sql(
-            #               normalize=False, pad=4, indent=4, dialect="snowflake", pretty=True
-            #           )
-            #            print(formatted_sql_query)
-
-            #            print(statement)
-
-            # Get the table/dynamic table/view name we're creating
-            child_object = str(statement.this)
+        if isinstance(statement, exp.Create) or isinstance(statement, exp.Alter):
+            # Get the table/dynamic table/view name we're creating/altering
+            node_name = str(statement.this)
 
             root = build_scope(statement)
 
-            # Stolen from the sqlglot documentation
-            # https://github.com/tobymao/sqlglot/blob/main/posts/ast_primer.md#scope
-            tables = [
-                source
-                # Traverse the Scope tree, not the AST
-                for scope in root.traverse()
-                # `selected_sources` contains sources that have been selected in this scope, e.g. in a FROM or JOIN clause.
-                # `alias` is the name of this source in this particular scope.
-                # `node` is the AST node instance
-                # if the selected source is a subquery (including common table expressions),
-                #     then `source` will be the Scope instance for that subquery.
-                # if the selected source is a table,
-                #     then `source` will be a Table instance.
-                for alias, (node, source) in scope.selected_sources.items()
-                if isinstance(source, exp.Table)
-            ]
+            if not root:
+                # Just add the node to the graph if it's a standalone object
+                graph.add_node(node_name, script=script_name, object=node_name)
+                continue
+            else:
+                # Find all the tables/dynamic tables/views that are being referenced in the statement
 
-            table_strings = []
-            for table in tables:
-                table_strings.append(str(table.db) + "." + str(table.this))
+                # Stolen from the sqlglot documentation
+                # https://github.com/tobymao/sqlglot/blob/main/posts/ast_primer.md#scope
+                edge_names = [
+                    source
+                    for scope in root.traverse()
+                    for alias, (node, source) in scope.selected_sources.items()
+                    if isinstance(source, exp.Table)
+                ]
 
-            # Now make the list of tables unique
-            uniq_tables = list(set(table_strings))
-            # print(uniq_tables)
+                # Store related tables/dynamic tables/views (edges) in the graph
+                edge_strings = []
+                for edge in edge_names:
+                    edge_strings.append(str(edge.db) + "." + str(edge.this))
 
-            table_graph_edges = [(table, child_object) for table in uniq_tables]
-            # table_graph_edges = [(child_object, table) for table in uniq_tables]
-            #            return table_graph_edges
-            graph.add_node(child_object, script=script_name, table=child_object)
-            graph.add_edges_from(
-                table_graph_edges, script=script_name, table=child_object
-            )
+                # Now make the list of edges unique
+                uniq_edges = list(set(edge_strings))
 
+                # Add the node to the graph
+                graph.add_node(node_name, script=script_name, object=node_name)
 
-def get_dynamic_table_depends(sql: str) -> list[str]:
-    """
-    Get the list of tables that the provided SQL depends on.
-    Typically used to determine dependencies for a dynamic table or view.
-    """
-
-    parsed_statements = parse(sql, dialect="snowflake")
-
-    # Only process statements which create dynamic tables or views
-    for statement in parsed_statements:
-        # if statement.find_all(exp.Create):
-        if isinstance(statement, exp.Create):
-            #            formatted_sql_query = statement.sql(
-            #               normalize=False, pad=4, indent=4, dialect="snowflake", pretty=True
-            #           )
-            #            print(formatted_sql_query)
-
-            #            print(statement)
-
-            # Get the table name we're creating
-            #            print(repr(statement))
-            print(statement.this)
-
-            root = build_scope(statement)
-
-            #            for scope in root.traverse():
-            #                print(scope)
-
-            # Taken from the sqlglot documentation
-            tables = [
-                source
-                # Traverse the Scope tree, not the AST
-                for scope in root.traverse()
-                # `selected_sources` contains sources that have been selected in this scope, e.g. in a FROM or JOIN clause.
-                # `alias` is the name of this source in this particular scope.
-                # `node` is the AST node instance
-                # if the selected source is a subquery (including common table expressions),
-                #     then `source` will be the Scope instance for that subquery.
-                # if the selected source is a table,
-                #     then `source` will be a Table instance.
-                for alias, (node, source) in scope.selected_sources.items()
-                if isinstance(source, exp.Table)
-            ]
-
-            table_strings = []
-            for table in tables:
-                table_strings.append(str(table.db) + "." + str(table.this))
-                # print(repr(table))
-
-            # Now make the list of tables unique
-            uniq_tables = list(set(table_strings))
-            print(uniq_tables)
+                # Add the edges to the graph
+                graph_edges = [(edge, node_name) for edge in uniq_edges]
+                graph.add_edges_from(graph_edges, script=script_name, object=node_name)
 
 
 def format_sql(content):
     """
     Parses and formats the provided SQL content.
     """
-    #    from sqlglot import parse
 
     parsed_statements = parse(content, dialect="snowflake")
     formatted_sql_queries = ""
@@ -1165,7 +1088,7 @@ def get_schemachange_config(
     change_history_table,
     vars,
     create_change_history_table,
-    r_script_graph,
+    script_deps,
     baseline_version,
     no_unversioned_checksum,
     pretty,
@@ -1193,7 +1116,7 @@ def get_schemachange_config(
         "snowflake_schema": snowflake_schema,
         "change_history_table": change_history_table,
         "vars": vars,
-        "r_script_graph": r_script_graph,
+        "script_deps": script_deps,
         "create_change_history_table": create_change_history_table,
         "no_unversioned_checksum": no_unversioned_checksum,
         "baseline_version": baseline_version,
@@ -1229,7 +1152,7 @@ def get_schemachange_config(
         "snowflake_schema": None,
         "change_history_table": None,
         "vars": {},
-        "r_script_graph": False,
+        "script_deps": False,
         "create_change_history_table": False,
         "baseline_version": "",
         "pretty": False,
@@ -1452,8 +1375,6 @@ def main(argv=sys.argv):
         prog="schemachange",
         description="""Apply schema changes to a Snowflake account.
         Full readme at https://github.com/Snowflake-Labs/schemachange""",
-        # formatter_class=argparse.RawTextHelpFormatter,
-        # formatter_class=RichHelpFormatter,
         formatter_class=RawTextRichHelpFormatter,
     )
     subcommands = parser.add_subparsers(
@@ -1562,9 +1483,9 @@ def main(argv=sys.argv):
         )
 
     parser_deploy.add_argument(
-        "--r-script-graph",
+        "--script-deps",
         action="store_true",
-        help="Check dependencies for repeatable scripts and re-run all parent scripts in graph (default: %(default)s)",
+        help="Check dependencies for repeatable scripts and re-run all affected scripts (default: %(default)s)",
         required=False,
     )
 
@@ -1732,7 +1653,7 @@ def main(argv=sys.argv):
             "snowflake_database": None,
             "change_history_table": None,
             "snowflake_schema": None,
-            "r_script_graph": None,
+            "script_deps": None,
             "create_change_history_table": None,
             "no_unversioned_checksum": None,
             "baseline_version": None,
