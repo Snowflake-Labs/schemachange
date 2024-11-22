fd13a23b18317e0a790c7454466593015d514e93
Tiji Mathew <tiji.mathew@snowflake.com>
2024-11-22 10:04:05 -0500

    testing github actions using shell script file

---
+++ testSchemchange.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Script used in github actions to run test the schemachange functionality against the demo scenarios included in the repository.
+echo "::group::Setting up ${MY_TARGET_SCHEMA}"
+schemachange deploy --config-folder ./demo/${SCENARIO_NAME}/1_setup
+echo "::endgroup::"
+
+echo "::group::Testing Rendering to ${MY_TARGET_SCHEMA}"
+schemachange render --config-folder ./demo/${SCENARIO_NAME}/2_test ./demo/${SCENARIO_NAME}/2_test/A__render.sql
+schemachange render --config-folder ./demo/${SCENARIO_NAME}/2_test ./demo/${SCENARIO_NAME}/2_test/R__render.sql
+schemachange render --config-folder ./demo/${SCENARIO_NAME}/2_test ./demo/${SCENARIO_NAME}/2_test/V1.0.0__render.sql
+echo "::endgroup::"
+echo "::group::Testing Deployment using ${MY_TARGET_SCHEMA}"
+
+set +e
+schemachange deploy --config-folder ./demo/${SCENARIO_NAME}/2_test
+RESULT=$?
+
+if [ $RESULT -eq 0 ]; then
+    echo "Deployment Completed!"
+else
+    echo "Deployment Failed. Proceeding to Teardown."
+fi
+echo "::endgroup::"
+set -e
+
+echo "::group::Tearing down up ${MY_TARGET_SCHEMA}"
+schemachange deploy --config-folder ./demo/${SCENARIO_NAME}/3_teardown
+echo "::endgroup::"
+
+if [ $RESULT -ne 0 ]; then
+    exit 1
+fi
